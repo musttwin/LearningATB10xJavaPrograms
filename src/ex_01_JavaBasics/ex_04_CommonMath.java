0f0c81b222eb16c7f82beca7a027493b40347c6d
Siddhartha kabiraj <siddharthakabiraj@gmail.com>
2025-02-23 11:31:17 +0530

    comment lines

--- src/ex_01_JavaBasics/ex_04_CommonMath.java
+++ src/ex_01_JavaBasics/ex_04_CommonMath.java
@@ -1,7 +1,7 @@
 package ex_01_JavaBasics;
 
 public class ex_04_CommonMath {
-    public static void main(String Args[]){
+    public static void main(String[] Args){
         System.out.println(2*2);
         System.out.println(2/2);
         System.out.println(2%2);
