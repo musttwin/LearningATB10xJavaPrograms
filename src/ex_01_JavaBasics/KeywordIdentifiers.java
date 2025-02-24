2d4a5cb3f6109f7236f3d4faf679e99ac7555b2a
Siddhartha kabiraj <siddharthakabiraj@gmail.com>
2025-02-24 13:55:22 +0530

    Keyword and Identifiers

---
+++ src/ex_01_JavaBasics/KeywordIdentifiers.java
@@ -0,0 +1,19 @@
+package ex_01_JavaBasics;
+
+public class KeywordIdentifiers {
+    public static void main(String[] args) {
+        /*
+        Here
+        Main method is the predefined method of a Java Program.
+        it is the entry point for any java Program. The Main method has a specific signature. */
+
+        /*
+        Main Method Includes : -
+        Public is a Keyword
+        static is a Keyword
+        void is the return type
+        and as a parameter type of "String" and array named "args"
+         */
+        System.out.println("so this is the example of main method signature .......");
+    }
+}
