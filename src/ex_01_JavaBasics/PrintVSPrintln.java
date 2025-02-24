74a7172dbb48b3d7e84dd29ee56021b2bd4cbbbc
Siddhartha kabiraj <siddharthakabiraj@gmail.com>
2025-02-24 16:17:50 +0530

    PrintVSPrintln

---
+++ src/ex_01_JavaBasics/PrintVSPrintln.java
@@ -0,0 +1,13 @@
+package ex_01_JavaBasics;
+
+import javax.swing.*;
+
+public class PrintVSPrintln {
+    public static void main(String[] args) {
+        System.out.print("Hi Jack! ");
+        System.out.print("How are you ?");
+        System.out.println("Do you Know me ?");
+        System.out.println(" I AM SIDDHARTHA");
+        System.out.println("I am a CTS Employee");
+    }
+}
