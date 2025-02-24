6a1a4e996a78dd19d0ea11eafc128907a14d2262
Siddhartha kabiraj <siddharthakabiraj@gmail.com>
2025-02-24 16:00:16 +0530

    Exceptions

---
+++ src/ex_01_JavaBasics/ArithmaticException.java
@@ -0,0 +1,10 @@
+package ex_01_JavaBasics;
+
+public class ArithmaticException {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int Sum = a/b; // ArithmeticException
+        System.out.println(Sum);
+    }
+}
