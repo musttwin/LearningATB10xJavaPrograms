722e1bae38a3097f14e97f9e7477b225d088dd61
Siddhartha kabiraj <siddharthakabiraj@gmail.com>
2025-05-31 10:53:01 +0530

    PrintVSPrintln

---
+++ src/constructor.java
@@ -0,0 +1,23 @@
+//Class Object & constructor concept
+
+class Employee{
+    String name;
+    int id;
+
+    Employee(String name, int id){  //
+        this.name = name;
+        this.id = id;
+    }
+
+    public void employeeInfo(){
+        System.out.println(this.name);
+        System.out.println(this.id);
+    }
+}
+
+public class constructor{
+    public static void main(String[] args) {
+        Employee e1 = new Employee("SID", 32);
+        e1.employeeInfo();
+    }
+}
